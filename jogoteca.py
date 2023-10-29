39b4fdddc44ecdbedd485782c54b68278564aefb
rafaellkl11 <rafaelhenriqueif@gmail.com>
2023-10-29 18:05:10 -0300

    jogoteca2

--- jogoteca.py
+++ jogoteca.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash, url_for, abort
+import usuarios 
 
 app = Flask(__name__)
+app.secret_key = 'ifmg'
 
 @app.route('/')
 def index():
@@ -16,12 +18,21 @@ jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
 jogo2 = Jogo('God of War', 'Hack in Slash', 'Ps2')
 lista = [jogo1, jogo2]
 
+def usuario_logado():
+  return 'usuario_nome' in session
+
 @app.route('/novo')
 def novo():
+  if not usuario_logado():
+    abort(403)
+
   return render_template('novo.html', titulo_='Novo Jogo')
 
 @app.route('/criar', methods=['POST',])
 def criar():
+  if not usuario_logado():
+    abort(403)
+
   nome = request.form['nome']
   categoria = request.form['categoria']
   console = request.form['console']
@@ -29,5 +40,30 @@ def criar():
 
   lista.append(jogo)
   return redirect('/')
+
+@app.route('/login', methods=['GET', 'POST'])
+
+def login():
+  if request.method == 'POST' :
+    email = request.form['email']
+    senha = request.form['senha']
+    usuario = usuarios.buscar(email, senha)
+    if usuario is None: 
+      flash('Usuário/senha inválidos.')
+    else:
+      session['usuario_email'] = usuario.email 
+      session['usuario_nome'] = usuario.nome
+      return redirect(url_for('index')) 
+  return render_template('login.html')
+
+@app.route('/logout', methods=['POST'])
+def logout():
+  session.pop('usuario_email', None)
+  session.pop('usuario_nome', None)
+  return redirect(url_for('index'))
+
+@app.errorhandler(403)
+def acesso_negado(erro):
+  return render_template('acesso_negado.html'), 403
 
 app.run(host='0.0.0.0', debug=True)
---
+++ usuarios.py
@@ -0,0 +1,19 @@
+class Usuario:
+    def __init__(self, nome, email, senha):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+lista_usuarios = [
+    Usuario('Daniel', 'daniel@gmail.com', '123'),
+    Usuario('Natasha', 'natasha@gmail.com', 'abc')
+]
+
+dict_usuarios = {
+    usuario.email: usuario for usuario in lista_usuarios
+}
+def buscar(email, senha):
+    usuario = dict_usuarios.get(email)
+    if usuario != None and usuario.senha == senha: 
+        return usuario
+    else:
+        return None
